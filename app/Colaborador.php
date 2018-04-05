e27e5b3176c56d8c198f326f206d786a2167f905
Carlos Encine <carlosencinedotcom@gmail.com>
2018-04-05 14:37:24 -0300

    correção da model Colaborador

--- app/Colaborador.php
+++ app/Colaborador.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Colaborador extends Model
 {
+    protected $table = 'colaboradores';
     const CREATED_AT = 'data_cadastro';
     const UPDATED_AT = 'data_atualizacao';
 }
