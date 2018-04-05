115f6283b772fd8a4f86dccaade1d093af73a77f
Carlos Encine <carlosencinedotcom@gmail.com>
2018-04-05 14:30:20 -0300

    implementação doa controllers de colaboradores e de bancos

--- app/Http/Controllers/BancoController.php
+++ app/Http/Controllers/BancoController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class BancoController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
 }
--- app/Http/Controllers/ColaboradorController.php
+++ app/Http/Controllers/ColaboradorController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Colaborador;
 use Illuminate\Http\Request;
 
 class ColaboradorController extends Controller
 {
-    //
+    public function index()
+    {
+        $colaboradores = Colaborador::all();
+        return view('colaboradores.main', ['colaboradores' => $colaboradores]);
+    }
+
+    public function show($id)
+    {
+        $colaborador = Colaborador::where('sequencia', $id);
+        return view('colaboradores.show', ['colaborador', $colaborador]);
+    }
+
+    public function edit($id)
+    {
+        $colaborador = Colaborador::where('sequencia', $id);
+        return view('colaboradores.edit', ['colaborador', $colaborador]);
+    }
+
+    public function store(Request $request)
+    {
+        Colaborador::create($request->all());
+        return response()->json(['msg' => 'Colaborador inserido com sucesso']);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $colaborador = Colaborador::where('sequencia', $id);
+        $colaborador->update($request->all());
+        return response()->json(['msg' => 'Atualizado com sucesso!']);
+    }
+
+    public function delete($id)
+    {
+        Colaborador::delete($id);
+        return response()->json(['msg' => 'Excluido com sucesso']);
+    }
 }
